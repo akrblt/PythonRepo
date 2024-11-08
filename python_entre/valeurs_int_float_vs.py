ace44ebb4804d7ffc1660864e083993d852bbc55
akrblt <a.karabulut070@gmail.com>
2024-11-08 15:54:48 +0100

    .

---
+++ python_entre/valeurs_int_float_vs.py
@@ -0,0 +1,10 @@
+from ctypes.wintypes import SHORT
+
+from _testcapi import INT_MIN, LONG_MIN, LLONG_MAX, LONG_MAX, CHAR_MAX, CHAR_MIN
+
+print(INT_MIN)
+
+print(LONG_MIN)
+print(LONG_MAX)
+print(LLONG_MAX)
+print(CHAR_MIN)
